94737f0652efd583c1509891961e64b3c3aba529
Renato Cruz Souza <renato.souza.flooxmongagua@gmail.com>
2025-03-25 13:37:11 -0300

    feat: implement logic for inserting or updating menu items with optional photo upload

--- inc/menus.js
+++ inc/menus.js
@@ -1,3 +1,4 @@
+const { rejects } = require('assert')
 let conn = require('./db')
 let path = require('path')
 
@@ -32,30 +33,75 @@ module.exports = {
 
       fields.photo = `images/${path.parse(files.photo[0].filepath).base}`;
 
-     console.log("Fields Funcionando!!:", fields);
+      let query, queryPhoto = '', params = [
+        fields.title,
+        fields.description,
+        fields.price
+    ];
+
+        if(files.photo?.[0]?.filepath){
+
+            queryPhoto = `, photo = ?`;
+
+            params.push(fields.photo);
+        }
+
+        if (parseInt(fields.id) > 0) {
+
+            params.push(fields.id)            
+          
+            query = `
+        UPDATE tb_menus
+        SET title = ?,
+            description = ?,
+            price = ?
+            ${queryPhoto}
+        WHERE id = ?
+    `;
+        } else {
+
+            if (files.photo?.[0]?.filepath) {
+               return reject('Envie a foto do prato!')
+            }
+
+            query = `
+                INSERT INTO tb_menus (title, description, price, photo)
+                VALUES(?, ?, ?, ?)
+            `;
+        }
+
+        console.log("Caminho da foto:", fields.photo);
+
+        conn.query(query, params, (err, results) => {
+            if (err) {
+                reject(err);
+                console.error("Erro ao inserir ou atualizar no banco:", err);
+            } else {
+                resolve(results);
+            }
+        });
+    });
+},
+
+delete(id){
+
+    return new Promise((resolve, reject) =>{
       
-      console.log("Caminho da foto:", files.photo[0].filepath);
+        conn.query(`
+            DELETE FROM tb_menus WHERE id = ?
+            `, [
+                id
+            ], (err, results)=>{
 
-      conn.query(`
-          INSERT INTO tb_menus (title, description, price, photo)
-          VALUES(?, ?, ?, ?)
-        `,[
-          fields.title,
-          fields.description,
-          fields.price,
-          fields.photo
-        ],(err, results)=>{
+               if (err) {
+                    reject(err)
+               } else {
+                    resolve(results)
+               }
 
-          if (err) {
-            reject(err)
-            console.error("Erro ao inserir no banco:", err);
-          } else {
-            resolve(results)
-          }
-
-        })
-
+            })
     })
 
-  }
+}
+
 }
