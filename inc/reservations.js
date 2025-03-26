21ef2a9a244121f4186cfcd62b4757bff789f078
Renato Cruz Souza <renato.souza.flooxmongagua@gmail.com>
2025-03-26 13:17:54 -0300

    feat: add reservations CRUD functionality with improved data handling

--- inc/reservations.js
+++ inc/reservations.js
@@ -74,7 +74,54 @@ module.exports = {
 
         })
 
+    },
+
+    getReservations() {
+
+        return new Promise((resolve, reject)=>{
+    
+            conn.query(`
+        
+                SELECT * FROM tb_reservations ORDER BY date DESC
+                
+                `, (err, results) =>{
+            
+                  if(err) {
+                    reject(err)
+                  }
+                  
+                  resolve(results)
+            
+                }) 
+    
+        })
+    
+      }, 
+
+      delete(id){
+
+        return new Promise((resolve, reject) =>{
+          
+            conn.query(`
+                DELETE FROM tb_reservations WHERE id = ?
+                `, [
+                    id
+                ], (err, results)=>{
+    
+                   if (err) {
+                        reject(err)
+                   } else {
+                        resolve(results)
+                   }
+    
+                })
+        })
+    
     }
+    
+
+
+
 
 }
 
